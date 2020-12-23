aad0e0f9b069ae1fdd3240d2fef2daf6d28a6575
Derli Machado <dam.machado1@gmail.com>
2020-12-23 17:39:34 -0300

    Fix: refactoring everything.

--- Marketplace/console.py
+++ Marketplace/console.py
@@ -1,18 +1,107 @@
-from marketplace import Marketplace
-from categories import Category
+from marketplaces import Marketplace, Category, Subcategory
 
-marketplaces = ["mark1", "mark2", "mark3", "mark4", "mark5", "mark6"]
-categories = ["cat1", "cat2", "cat3", "cat4", "cat5", "cat6"]
-subCategories = ["subCat1", "subCat2", "subCat3", "subCat4"]
+mktplaces = [Marketplace('Amazon'), Marketplace('B2W'), Marketplace('Zoom'), Marketplace('Carrefour'), Marketplace('Sair')]
+categorias = [Category('Móveis', mktplaces[1]), Category('Telefonia', mktplaces[0]), Category('Eletrodomésticos', mktplaces[1])]
+subcategorias = [Subcategory('Sofá', categorias[0]), Subcategory('Mesa', categorias[0]), Subcategory('Mesa', categorias[1])]
 
-def list_marketplaces():
-    print('\nMarketplaces: ')    
-    for m in marketplaces:
-        print(m)
-list_marketplaces()
+def menu(): 
 
-def list_categories():
-    print('\nCategories: ')    
-    for c in categories:
-        print(c)
-list_categories()
+    print('\nMENU: ')
+
+    i = 1
+    for option in mktplaces:
+        print(f'{i} - {option}')
+        i += 1
+
+    op = int(input('Selecione uma opção: '))
+    return op
+
+while True:
+    try:
+        op = menu()
+        if op == 1:
+            item = 1
+            print(f'\nVocê escolheu a opção {mktplaces[0]}\n')
+            for i in categorias:
+                if i.get_parentname() ==  mktplaces[0].get_name():
+                    print(f'{item} - {i.get_name()}')
+                    item += 1
+            escolher_cat = int(input(f'\nDigite uma opção de categoria: '))
+            while True:
+                try:
+                    if escolher_cat == 1:
+                        for i in subcategorias:
+                            if i.get_parentname() == categorias[1].get_name():
+                                print(f'\n Menu de categorias:')
+                                print(f'\n{i.get_subcat()}')
+                        break
+                except Exception as e: 
+                    print(e)
+                    break
+                
+        elif op == 2:
+            item = 1
+            print(f'\nVocê escolheu a opção {mktplaces[1]}\n')
+            for i in categorias:
+                if i.get_parentname() ==  mktplaces[1].get_name():
+                    print(f'{item} - {i.get_name()}')
+                    item += 1
+            escolher_cat = int(input(f'\nDigite uma opção de categoria: '))
+            while True:
+                try:
+                    if escolher_cat == 1:
+                        for i in subcategorias:
+                            if i.get_parentname() == categorias[0].get_name():
+                                print(f'\n{i.get_subcat()}')
+
+                        break
+                except Exception as e: 
+                    print(e)
+                    break
+                
+        elif op == 3:
+            item = 1
+            print(f'\nVocê escolheu a opção {mktplaces[2]}\n')
+            for i in categorias:
+                if i.get_parentname() ==  mktplaces[2].get_name():
+                    print(f'{item} - {i.get_name()}')
+                    item += 1
+            escolher_cat = int(input(f'\nDigite uma opção de categoria: '))
+            while True:
+                try:
+                    if escolher_cat == 1:
+                        for i in subcategorias:
+                            if i.get_parentname() == categorias[0].get_name():
+                                print(f'\n{i.get_subcat()}')
+                        break
+                except Exception as e: 
+                    print(e)
+                    break
+
+        elif op == 4:
+            item = 1
+            print(f'\nVocê escolheu a opção {mktplaces[3]}\n')
+            for i in categorias:
+                if i.get_parentname() ==  mktplaces[3].get_name():
+                    print(f'{item} - {i.get_name()}')
+                    item += 1
+            escolher_cat = int(input(f'\nDigite uma opção de categoria: '))
+            while True:
+                try:
+                    if escolher_cat == 1:
+                        for i in subcategorias:
+                            if i.get_parentname() == categorias[0].get_name():
+                                print(f'\n{i.get_subcat()}')
+                        break
+                except Exception as e: 
+                    print(e)
+                    break
+            
+        elif op == 5:
+            exit(0) #tentar alterar
+        else:
+            print('Digite uma opção válida!')
+        break
+
+    except ValueError:
+        print('Opção indisponível. Tente novamente.')
--- Marketplace/web.py
+++ Marketplace/web.py
@@ -1,10 +1,22 @@
-from flask import Flask
-
+# pip3 install flask
+# which pip3 -  caminho da instalacao do pip3
+from flask import Flask, render_template
+from marketplaces import Marketplace, Category, Subcategory
 
 app = Flask(__name__)
 
-@app.route('/')
+mktplaces = [Marketplace('Amazon'), Marketplace('B2W'), Marketplace('Zoom'), Marketplace('Carrefour'), Marketplace('Sair')]
+categorias = [Category('Móveis', mktplaces[1]), Category('Telefonia', mktplaces[0]), Category('Eletrodomésticos', mktplaces[1])]
+subcategorias = [Subcategory('Sofá', categorias[0]), Subcategory('Mesa', categorias[0]), Subcategory('Mesa', categorias[1])]
 
-def home():
-  h1 = '<h1>List all marketplaces</h1>'
-  
+
+@app.route('/')
+def index():
+    return render_template('index.html', mktplaces = mktplaces)
+
+@app.route('/category/<mkt>')
+def category(mkt):
+    return render_template('category.html', cat = categorias, mkt = mkt)
+
+
+app.run(debug=True)
