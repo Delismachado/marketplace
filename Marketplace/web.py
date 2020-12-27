4b4237009f551523b28b22f08c444a8bf772c3a3
Derli Machado <dam.machado1@gmail.com>
2020-12-27 09:04:34 -0300

    add: styles.

--- Marketplace/web.py
+++ Marketplace/web.py
@@ -1,22 +1,24 @@
-# pip3 install flask
-# which pip3 -  caminho da instalacao do pip3
 from flask import Flask, render_template
-from marketplaces import Marketplace, Category, Subcategory
+from catalogo import Marketplace, Category, Subcategory
 
 app = Flask(__name__)
 
-mktplaces = [Marketplace('Amazon'), Marketplace('B2W'), Marketplace('Zoom'), Marketplace('Carrefour'), Marketplace('Sair')]
-categorias = [Category('Móveis', mktplaces[1]), Category('Telefonia', mktplaces[0]), Category('Eletrodomésticos', mktplaces[1])]
-subcategorias = [Subcategory('Sofá', categorias[0]), Subcategory('Mesa', categorias[0]), Subcategory('Mesa', categorias[1])]
-
+marketplaces = [Marketplace('Enjoei'), Marketplace('BigCommerce'), Marketplace('Etsy'), Marketplace('Evolutto'), Marketplace('Arb')]
+categories = [Category('Home', marketplaces[0]), Category('Dinner', marketplaces[1]), Category('Food', marketplaces[2])]
+subcategories = [Subcategory('Lunch', categories[0]), Subcategory('Style', categories[1]), Subcategory('Go', categories[2])]
 
 @app.route('/')
 def index():
-    return render_template('index.html', mktplaces = mktplaces)
+    return render_template('index.html', marketplaces = marketplaces)
 
 @app.route('/category/<mkt>')
 def category(mkt):
-    return render_template('category.html', cat = categorias, mkt = mkt)
+    return render_template('category.html', cat = categories, mkt = mkt)
+
+@app.route('/subcategories/<cat>')
+def subcategory(cat):
+    return render_template('subcategories.html', subcat = subcategories, cat = cat)
+
 
 
 app.run(debug=True)
